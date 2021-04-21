5fccc39c446b3437a38290a8c82f20422d0cc6e3
Erik Ussin <e.ussin@outlook.com>
2021-04-21 21:36:16 +0200

    Local IP is sent with every Async Request (Requires latest Ryder Engine Version)

--- Ryder Display/Network/Client.py
+++ Ryder Display/Network/Client.py
@@ -9,30 +9,31 @@ class Client(object, metaclass=Singleton):
     def __init__(self):
         self._url = ''
         self._timeout = 1.0
+        self._ip = socket.gethostbyname(socket.gethostname())
 
     def setUrl(self, ip, port):
         self._url = 'http://' + ip + ':' + str(port)
 
     def subscribeToRyderEngine(self):
-        Client.sendQuery(self._url, { "request":"subscribe" }, self._timeout)
+        Client.sendQuery(self._url, { "request":"subscribe", "ip":self._ip }, self._timeout)
 
     def querySteamLogin(self):
-        Client.sendQuery(self._url, { "request":"steamLoginUP" }, self._timeout)
+        Client.sendQuery(self._url, { "request":"steamLoginUP", "ip":self._ip }, self._timeout)
 
     def querySteam2FA(self):
-        Client.sendQuery(self._url, { "request":"steamLogin2FA" }, self._timeout)
+        Client.sendQuery(self._url, { "request":"steamLogin2FA", "ip":self._ip }, self._timeout)
 
     def queryDiscordLogin(self):
-        Client.sendQuery(self._url, { "request":"discordLogin" }, self._timeout)
+        Client.sendQuery(self._url, { "request":"discordLogin", "ip":self._ip }, self._timeout)
 
     def request_system_status(self):
         Client.sendQuery(self._url, { "request":"status" }, self._timeout)
 
     def requestForegroundProcessName(self):
-        Client.sendQuery(self._url, { "request":"foregroundProcess" }, self._timeout)
+        Client.sendQuery(self._url, { "request":"foregroundProcess", "ip":self._ip }, self._timeout)
 
     def requestForegroundProcessIcon(self):
-        Client.sendQuery(self._url, { "request":"foregroundProcessIcon" }, self._timeout)
+        Client.sendQuery(self._url, { "request":"foregroundProcessIcon", "ip":self._ip }, self._timeout)
 
     def sendQuerySync(url, data, timeout):
         try:
