b534a767fbcdd1e70bc9abf0cccaf2200291967f
Erik Ussin <e.ussin@outlook.com>
2021-04-23 20:02:43 +0200

    Adressed reconnection issues

--- Ryder Display/Network/RyderClient.py
+++ Ryder Display/Network/RyderClient.py
@@ -26,39 +26,44 @@ class RyderClient(object, metaclass=Singleton):
             self._s.sendall((data+"\n").encode('utf-8'))
             return True
         except:
-            print("Ryder Engine not connected")
+            print("Could not send: " + data)
             return False
 
     def run(self):
         data = ''
         last_update = -9999
         self._stop = False
+        connected = False
         while not self._stop:
-            # Attempt reconnection to Ryder Engine after 10 seconds of no data
-            if time.time() - last_update >= 10:
-                try:
-                    print("Attempt Ryder Engine connection")
-                    if self._s is not None:
-                        self._s.close()
-                    self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    self._s.connect((self._ip, self._port))
-                    self._s.settimeout(0.001)
-                    # Call functions that are meant to run when connection is established
-                    if 'on_connect' in self._endpoints:
-                        print("Endpoint: on_connect")
-                        for endpoint in self._endpoints['on_connect']:
-                            endpoint()
-                except:
-                    pass
+            # Attempt connection to Ryder Engine
+            try:
+                print("Connecting to Ryder Engine...")
+                if self._s is not None:
+                    self._s.close()
+                self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self._s.settimeout(2.5)
+                self._s.connect((self._ip, self._port))
+                self._s.settimeout(0.001)
+                # Call functions that are meant to run when connection is established
+                if 'on_connect' in self._endpoints:
+                    print("Endpoint: on_connect")
+                    for endpoint in self._endpoints['on_connect']:
+                        endpoint()
+                # Reset variables
+                connected = True
                 last_update = time.time()
                 data = ''
                 last_data_size = 0
+            except:
+                pass
+            
             # Loop to receive messages
-            while not self._s._closed or not self._stop:
+            while not self._stop and connected:
                 try:
                     data += self._s.recv(2056).decode("utf-8") 
                     # Check for inactivity timeout
-                    if last_data_size == len(data) and time.time() - last_update >= 10:
+                    if len(data) == 0:
+                        connected = False
                         break
                     else:
                         last_update = time.time()
@@ -77,5 +82,7 @@ class RyderClient(object, metaclass=Singleton):
                     last_data_size = len(data)
                 except:
                     if time.time() - last_update >= 10:
+                        connected = False
                         break
-            gevent.sleep(0.01)
+                gevent.sleep(0.025)
+            gevent.sleep(0.025)
