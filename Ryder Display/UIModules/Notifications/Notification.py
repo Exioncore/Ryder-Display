96fd4c4c48e34bb36faf220bcf2b237ab333dddb
Erik Ussin <e.ussin@outlook.com>
2021-06-16 07:33:44 +0200

    Updated NotificationHandler to handle visualizing more than 1 notification at once. Added monkey patching to fix steam integration.

--- Ryder Display/UIModules/Notifications/Notification.py
+++ Ryder Display/UIModules/Notifications/Notification.py
@@ -6,38 +6,62 @@ from PyQt5.QtWidgets import QWidget, QLabel
 from PyQt5.QtGui import QColor, QPainter, QPen, QPixmap, QPainterPath, QBrush, QFont, QPalette
 
 class Notification(object):
-    def __init__(self, window, stylesheet=["","",""], img_margin = 5, pos=[0,0], size=[100,20], path=''):
+    def __init__(self, window, stylesheet=["","",""], img_margin = 5, pos=[0,0], size=[100,20], path='', vals=None):
         self._path = path
+        self._pos = pos
+
+        # Check if initialization values are present
+        if vals != None:
+            self._currApp = vals[0]
+            if vals[0] == "Discord":
+                logo_path = self._path+'/Resources/Discord-Logo.svg'
+                color = '#36393f'
+            elif vals[0] == "Steam":
+                logo_path = self._path+'/Resources/Steam-Logo.svg'
+                color = '#1b1c20'
+            else:
+                color = '#32a852'
+            title = vals[1]
+            message = vals[2]
+        else:
+            # Default values 
+            self._currApp = 'null'
+            logo_path = self._path+'/Resources/Steam-Logo.svg'
+            color = '#1b1c20'
+            title = 'Steam'
+            message = 'Demo Steam Notification'
+
+        # Create components
+        ### Notification Background
         self._background_main_stylesheet = stylesheet[0]
         self._background = QLabel(window)
         self._background.setGeometry(pos[0], pos[1], size[0], size[1])
-        self._background.show()
-
-        self._logo = QSvgWidget(path+'/Resources/Discord-Logo.svg', self._background)
-        print(path+'/Resources/Discord-Logo.svg')
+        ### Notification Logo
+        self._logo = QSvgWidget(logo_path, self._background)
         self._logo.setGeometry(
             img_margin, img_margin,
             size[1] - img_margin*2, size[1] - img_margin*2
         )
+        self._logo.setStyleSheet('background-color:'+color+';')
         self._logo.show()
-
+        ### Notification Title
         self._title = QLabel(self._background)
         self._title.setAttribute(Qt.WA_TranslucentBackground)
         self._title.setAlignment(Qt.AlignTop | Qt.AlignLeft)
         self._title.setStyleSheet('QLabel{'+stylesheet[1]+'}')
-        self._title.setText('Title')
+        self._title.setText(title)
         self._title.adjustSize()
         self._title.setGeometry(
             img_margin + self._logo.width() + 4, img_margin,
             size[0] - img_margin - self._logo.width() - 4, self._title.height()
         )
         self._title.show()
-
+        ### Notification Message
         self._message = QLabel(self._background)
         self._message.setAttribute(Qt.WA_TranslucentBackground)
         self._message.setAlignment(Qt.AlignTop | Qt.AlignLeft)
         self._message.setStyleSheet('QLabel{'+stylesheet[2]+'}')
-        self._message.setText('Message')
+        self._message.setText(message)
         self._message.adjustSize()
         self._message.setGeometry(
             img_margin + self._logo.width() + 8, img_margin + self._title.height() + 2,
@@ -45,18 +69,26 @@ class Notification(object):
         )
         self._message.show()
 
-    def setText(self, app, title, message):
-        if app == "Discord":
-            self._logo.load(self._path+'/Resources/Discord-Logo.svg')
-            color = '#36393f'
-        elif app == "Steam":
-            self._logo.load(self._path+'/Resources/Steam-Logo.svg')
-            color = '#1b1c20'
+        if vals != None:
+            self._background.show()
         else:
-            color = '#32a852'
+            self._background.hide()
 
-        self._background.setStyleSheet('QLabel {background-color:'+color+';'+self._background_main_stylesheet+'}')
-        self._logo.setStyleSheet('background-color:'+color+';')
+    def setText(self, app, title, message):
+        if self._currApp != app:
+            if app == "Discord":
+                self._logo.load(self._path+'/Resources/Discord-Logo.svg')
+                color = '#36393f'
+            elif app == "Steam":
+                self._logo.load(self._path+'/Resources/Steam-Logo.svg')
+                color = '#1b1c20'
+            else:
+                color = '#32a852'
+            self._background.setStyleSheet('QLabel {background-color:'+color+';'+self._background_main_stylesheet+'}')
+            self._logo.setStyleSheet('background-color:'+color+';')
+            self._currApp = app
+
+        # Update Textual Contents
         self._title.setText(title)
         self._message.setText(message)
 
@@ -73,4 +105,16 @@ class Notification(object):
         self._background.hide()
 
     def move(self, x, y):
+        self._pos = [x, y]
         self._background.move(x, y)
+
+    def moveX(self, x):
+        self._pos[0] = x
+        self._background.move(x, self._pos[1])
+
+    def moveY(self, y):
+        self._pos[1] = y
+        self._background.move(self._pos[0], y)
+
+    def bringToBack(self):
+        self._background.lower()
--- Ryder Display/UIModules/Notifications/NotificationsHandler.py
+++ Ryder Display/UIModules/Notifications/NotificationsHandler.py
@@ -2,6 +2,8 @@ import math
 import gevent
 from threading import Lock
 
+from PyQt5.QtWidgets import QWidget
+
 from Network.Hyperion import Hyperion
 from Utils.Transitioner import Transitioner
 from Network.RyderClient import RyderClient
@@ -11,28 +13,44 @@ from UIModules.Notifications.Notification import Notification
 
 class NotificationsHandler(object):
     _mutex : Lock
+    _mutexUpdate : Lock
+    _live_queue = []
     _queue = []
+    _free_notifications = []
+    _slot = 0
     _timer = 0
     _onGoing = False
     
     def __init__(self, window, fps, settings,
-                stylesheet=["","",""], img_margin = 5, pos='bottom', height=20, path=''
+                stylesheet=["","",""], img_margin = 5, pos='bottom', height=20, path='',
         ):
+        self._mutexUpdate = Lock()
         self._mutex = Lock()
         self._timeout_frames = math.floor(fps * settings['timeout_seconds'])
         self._transition_frames = math.floor(fps * settings['transition_seconds'])
+        if 'max_n_notifications' in settings:
+            max_stack = settings['max_n_notifications']
+        else:
+            max_stack = 1
 
         if pos == 'top':
-            pos = [0, -height]
             self._ofst = height
+            self._init_pos = [0, 0]
         else:
-            pos = [0, window.height()]
             self._ofst = -height
-        size = [window.width(), height]
+            self._init_pos = [0, window.height() + self._ofst]
+        self._size = [window.width(), height]
 
-        # Notification UI
-        self._notification_t = Transitioner(pos[1], self._transition_frames)
-        self._notification = Notification(window, stylesheet, img_margin, pos, size, path)
+        # Notifications UI
+        ### Create container (This is necessary in order to have z-order of each notification indipendent
+        ### from the rest of ui elements
+        container = QWidget(window)
+        container.setFixedSize(window.width(), window.height())
+        ### Create notifications
+        for i in range(max_stack):
+            self._free_notifications.append(
+                Notification(container, stylesheet, img_margin, self._init_pos, self._size, path)
+        )
 
         # Steam
         if settings['steam']['enabled']:
@@ -66,42 +84,74 @@ class NotificationsHandler(object):
             self._discord_ui_notify = self._discord_hyperion_effect = False
 
     def update(self, status=None):
-        # No notifications going thus check for new ones to display
-        if not self._onGoing:
-            if len(self._queue) > 0:
-                self._mutex.acquire()
-                entry = self._queue.pop(0)
-                self._mutex.release()
-                # UI Notification
-                if ((entry[0] == 'Steam' and self._steam_ui_notify) or
-                    (entry[0] == 'Discord' and self._discord_ui_notify)):
-                    self._notification.setText(entry[0], entry[1], entry[2])
-                    self._notification.move(0, self._notification_t.start)
-                    self._notification_t.transitionFromStart(self._ofst, self._transition_frames)
-                    self._notification.show()
-                # Hyperion Notification
-                if Hyperion().ledState and Hyperion().notifications:
-                    if entry[0] == 'Steam'  and self._steam_hyperion_effect != False:
-                        Hyperion().setEffect(self._steam_hyperion_effect, 1, 0)
-                    elif entry[0] == 'Discord' and self._discord_hyperion_effect != False:
-                        Hyperion().setEffect(self._discord_hyperion_effect, 1, 0)
-                self._onGoing = True
-        # Timer Handling
-        if self._onGoing:
-            if not self._notification_t.isDone():
-                self._notification.move(0, self._notification_t.update())
-                self._notification.update()
+        self._mutexUpdate.acquire()
+        # Process transitioner updates
+        for item in self._live_queue:
+            # Perform update if necessary
+            if not item['transitioner'].isDone():
+                item['notification'].moveY(item['transitioner'].update())
+                item['notification'].update()
+
+        # Process current live notifications
+        i = 0
+        while i < len(self._live_queue):
+            item = self._live_queue[i]
+            if item['stackPos'] != i and item['transitioner'].isDone():
+                item['transitioner'] = Transitioner(
+                    item['notification']._pos[1], self._transition_frames
+                )
+                item['transitioner'].transitionFromStart(-self._ofst, self._transition_frames)
+                item['stackPos'] = i
+                item['notification']._background.raise_()
+            elif self._timer - item['timeOfCreation'] >= self._timeout_frames and item['stackPos'] == 0 and item['transitioner'].isDone():
+                # Delete notification
+                item['notification'].hide()
+                self._free_notifications.append(self._live_queue.pop(0)['notification'])
+                self._slot = self._slot - 1
+                continue
+            i = i + 1
+
+        # Check if there are new notifications to display
+        while len(self._queue) > 0 and len(self._free_notifications) > 0:
+            slot = self._slot
+            self._slot = self._slot + 1
+            # Transition from the right side of the screen
+            if slot > 0:
+                startPosY = self._live_queue[len(self._live_queue) - 1]['notification']._pos[1]
+                delta = self._ofst * slot - startPosY
             else:
-                self._timer += 1
-            if self._timer >= self._timeout_frames:
-                self._notification.hide()
-                self._timer = 0
-                self._onGoing = False
-                self.update()
+                startPosY = self._init_pos[1] - self._ofst
+                delta = self._ofst * (slot + 1)
+            notification_t = Transitioner(startPosY, self._transition_frames)
+            notification_t.transitionFromStart(delta, self._transition_frames)
+            # Initialize notification
+            self._mutex.acquire()
+            data = self._queue.pop(0)
+            self._mutex.release()
+            notification = self._free_notifications.pop(0)
+            notification.setText(data[0], data[1], data[2])
+            notification.move(self._init_pos[0], startPosY)
+            notification.update()
+            notification.bringToBack()
+            notification.show()
+            # Add to live queue Notification, Notification Transitioner, position in stack, creation time
+            self._live_queue.append({
+                'notification': notification,
+                'transitioner': notification_t,
+                'stackPos': slot,
+                'timeOfCreation': self._timer
+            })
+            # Hyperion Notification
+            if Hyperion().ledState and Hyperion().notifications:
+                if data[0] == 'Steam'  and self._steam_hyperion_effect != False:
+                    Hyperion().setEffect(self._steam_hyperion_effect, 1, 0)
+                elif data[0] == 'Discord' and self._discord_hyperion_effect != False:
+                    Hyperion().setEffect(self._discord_hyperion_effect, 1, 0)
+
+        self._timer = self._timer + 1
+        self._mutexUpdate.release()
 
     def newNotification(self, app, title, message):
         self._mutex.acquire()
         self._queue.append([app, title, message])
         self._mutex.release()
-        if not self._onGoing:
-            self.update()
